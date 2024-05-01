f0fae65d4fe0e679ab9049683466b393e9dd10a7
Jesse Schulman <jesse@caleos.io>
2024-05-01 16:57:29 -0700

    Change wasi randomness source to work on Antelope chains via tapos block prefix

--- src/wasi.rs
+++ src/wasi.rs
@@ -1,17 +1,34 @@
 //! Implementation for WASI
-use crate::Error;
 use core::{
-    mem::MaybeUninit,
-    num::{NonZeroU16, NonZeroU32},
+    mem::MaybeUninit
 };
-use wasi::random_get;
+use crate::Error;
+
+mod intrinsics {
+    extern "C" {
+        pub fn tapos_block_prefix() -> i32;
+    }
+}
+
+pub fn tapos_block_prefix() -> u32 {
+    return unsafe { intrinsics::tapos_block_prefix() as u32 };
+}
 
 pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
-    unsafe { random_get(dest.as_mut_ptr() as *mut u8, dest.len()) }.map_err(|e| {
-        // The WASI errno will always be non-zero, but we check just in case.
-        match NonZeroU16::new(e.raw()) {
-            Some(r) => Error::from(NonZeroU32::from(r)),
-            None => Error::ERRNO_NOT_POSITIVE,
-        }
-    })
+    let block_prefix = tapos_block_prefix();
+    let bytes = block_prefix.to_le_bytes();
+
+    // Copy the bytes into the destination slice.
+    for (i, byte) in bytes.iter().enumerate() {
+        dest[i] = MaybeUninit::new(*byte);
+    }
+
+    Ok(())
+    // unsafe { random_get(dest.as_mut_ptr() as *mut u8, dest.len()) }.map_err(|e| {
+    //     // The WASI errno will always be non-zero, but we check just in case.
+    //     match NonZeroU16::new(e.raw()) {
+    //         Some(r) => Error::from(NonZeroU32::from(r)),
+    //         None => Error::ERRNO_NOT_POSITIVE,
+    //     }
+    // })
 }
